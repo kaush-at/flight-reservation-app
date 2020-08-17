376817d75cda10e90f9dde0a5c7eed0ea7171273
kaush <emailtokaush@gmail.com>
2020-08-17 10:01:58 -0400

    disable whitelable errors and add customize error pages

---
+++ flightreservation/src/main/java/com/kaush/flightreservation/controllers/AppErrorController.java
@@ -0,0 +1,39 @@
+package com.kaush.flightreservation.controllers;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppErrorController implements ErrorController{
+
+	@Override
+	public String getErrorPath() {
+		// TODO Auto-generated method stub
+		return "/error";
+	}
+	
+	@GetMapping("/error")
+	public String handlingError(HttpServletRequest request) {
+		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		
+		if(status != null) {
+			Integer statusCode = Integer.valueOf(status.toString());
+			
+			if(statusCode == HttpStatus.NOT_FOUND.value()) {
+				return "errorpages/error-404";
+			}else if(statusCode == HttpStatus.FORBIDDEN.value()) {
+				return "errorpages/error-403";
+			}else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+				return "errorpages/error-500";
+			}
+		}
+		
+		return "errorpages/error";
+	}
+
+}
